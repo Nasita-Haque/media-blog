ef723905516bbdd04972f2618f938c576e172db9
Crazy Kiwi <Nasita-Haque>
2016-12-30 22:26:40 -0500

    Updadd post-routes

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// router.use('/post', require('./post-routers.js'));
+router.use('/post', require('./post-routes.js'));
 router.use('/users', require('./user-routes.js'));
 
 module.exports = router;
--- routers/post-routes.js
+++ routers/post-routes.js
@@ -1,7 +1,8 @@
-const postRoutes = require('express').Routes()
+const postRoutes = require('express').Router();
 const sequelize = require('../models/index.js');
 const models = require('../models');
 
+//Get all posts
 let getPost = ((req, res)=>{
 	models.Post.findAll({
 		include: [models.Comment],
@@ -12,19 +13,95 @@ let getPost = ((req, res)=>{
 		res.send(post);
 	})
 	.catch((error)=>{
-		res.error('Error =>', error)
+		res.status(500).send('Error. Check routes.')
+		console.log('Error=>', error)
 	})
 });
 
-let postPost = ((req, res)=>{
-	models.Post.create({
-		title: req.body.title
-		body: req.body.body
+
+//Get all posts from a user 
+let getUserPost = ((req, res)=>{
+	models.Post.findAll({
+		where: {UserId: req.params.userId},
+		include: [{model: models.Comment}],
+		include: [{model: models.Prefecture}],
+		include: [{model: models.Tag}]
+	})
+	.then((post)=>{
+		res.send(post)
+	})
+	.catch((error)=>{
+		res.status(500).send('Error. Check routes.')
+		console.log('Error=>', error)
 	})
 })
 
-let deletePost = 
 
+//Create a post under a user: 
+	// title:
+	// userId:
+	// prefecture:
+	// body:
+	// tag:
+let postPost = ((req, res)=>{
+	let postId; 
+	models.Prefecture.findOrCreate({
+		where: {
+			prefecture: req.body.prefecture
+		}
+	})
+	.then((prefecture)=>{
+		return models.Post.create({
+			title: req.body.title,
+			body: req.body.body,
+			UserId: req.body.userId,
+			PrefectureId: prefecture[0].id
+		})
+	})
+	.then((post)=>{
+		console.log('post=>', post)
+		 postId = post.id
+	})
+	.then(()=>{
+		console.log('postId=>', postId)
+		models.Tag.findOrCreate({
+			where: {
+				tag: req.body.tag,
+				PostId: postId
+			}
+		})
+	})
+	.then(()=>{
+		res.status(200).send('Post successfully created.')
+	})
+	.catch((error)=>{
+		res.status(500).send('Error. Check routes.')
+		console.log('Error=>', error)
+	})
+});
+
+
+//Delete a user post
+let deletePost = ((req, res)=>{
+	models.Post.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+})
+
+postRoutes.route('/')
+	.get(getPost)
+	.post(postPost)
+
+postRoutes.route('/:userId')
+	.get(getUserPost)
+
+postRoutes.route('/:id')
+	.delete(deletePost)
+
+
+module.exports = postRoutes; 
 
 
 
